ba166d1da67f8636a57847b681baa0a53f48106b
Yazeed Bzadough <yazeedb@users.noreply.github.com>
2018-07-25 21:07:57 -0400

    loading indicator

--- src/User/index.js
+++ src/User/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { componentFromStream } from 'recompose';
-import { of } from 'rxjs';
+import { BehaviorSubject, merge, of } from 'rxjs';
 import {
   catchError,
   debounceTime,
+  delay,
   filter,
   map,
   pluck,
-  switchMap
+  switchMap,
+  tap
 } from 'rxjs/operators';
 import { ajax } from 'rxjs/ajax';
 import Error from '../Error';
@@ -16,20 +18,28 @@ import './User.css';
 
 const formatUrl = user => `https://api.github.com/users/${user}`;
 
-const User = componentFromStream(prop$ =>
-  prop$.pipe(
+const User = componentFromStream(prop$ => {
+  const loading$ = new BehaviorSubject(false);
+  const getUser$ = prop$.pipe(
     debounceTime(1000),
     pluck('user'),
     filter(user => user && user.length),
     map(formatUrl),
+    tap(() => loading$.next(true)),
     switchMap(url =>
       ajax(url).pipe(
         pluck('response'),
+        delay(1500),
         map(Component),
+        tap(() => loading$.next(false)),
         catchError(error => of(<Error {...error} />))
       )
     )
-  )
-);
+  );
+
+  return merge(loading$, getUser$).pipe(
+    map(result => (result === true ? <p>Loading...</p> : result))
+  );
+});
 
 export default User;
