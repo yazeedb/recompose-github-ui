363531b21e0a2e2010ded8a86708a587118209b3
yazeedb <ybzadough@gmail.com>
2018-07-26 06:56:21 -0400

    begin index.js

--- src/index.js
+++ src/index.js
@@ -1,28 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { componentFromStream, createEventHandler } from 'recompose';
-import { combineLatest } from 'rxjs';
-import { map, startWith } from 'rxjs/operators';
-import User from './User';
-import './observableConfig';
 import './styles.css';
 
-const App = componentFromStream(prop$ => {
-  const { handler, stream } = createEventHandler();
-  const value$ = stream.pipe(
-    map(e => e.target.value.trim()),
-    startWith('')
-  );
-
-  return combineLatest(prop$, value$).pipe(
-    map(([props, value]) => (
-      <div>
-        <input onChange={handler} placeholder="GitHub username" />
-        <User user={value} />
-      </div>
-    ))
-  );
-});
+const App = () => (
+  <div>
+    <input placeholder="GitHub username" />
+  </div>
+);
 
 const rootElement = document.getElementById('root');
 ReactDOM.render(<App />, rootElement);
